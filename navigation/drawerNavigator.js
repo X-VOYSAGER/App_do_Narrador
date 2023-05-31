a7e4a890058467528d9cf970cb9ef0ffa5a6206a
X-VOYSAGER <pjsandrade08@gmail.com>
2023-05-31 16:11:04 -0300

    Narrador V2

--- navigation/drawerNavigator.js
+++ navigation/drawerNavigator.js
@@ -8,11 +8,10 @@ const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigator() {
   return (
-    <NavigationContainer>
+    
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={TabNavigator} />
         <Drawer.Screen name="Profile" component={Profile} />
       </Drawer.Navigator>
-    </NavigationContainer>
   );
 }
